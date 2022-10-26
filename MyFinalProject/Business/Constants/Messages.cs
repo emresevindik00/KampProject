08afa3339c2f88acda8531a8307b1f7561f99d10
emresevindik00 <emre.sevindik99@gmail.com>
2022-10-26 14:13:24 +0300

    project finished

---
+++ MyFinalProject/Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExist = "Böyle bir ürün zaten var";
+        internal static string CheckIfCategoryLimitExceded = "Kategori limiti aşıldı";
+        internal static string AuthorizationDenied = "Yetkiniz yok";
+    }
+}
